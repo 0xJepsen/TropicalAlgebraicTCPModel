1695e6bf8a731b7e6b02f4067d3f25efa3691bb0
Waylon Jepsen <57912727+0xJepsen@users.noreply.github.com>
2021-11-27 16:46:42 -0700

    organization and link testing.

--- Max-Plus model/main.py
+++ Max-Plus model/main.py
@@ -1,12 +1,13 @@
 from random import expovariate
 from scipy.stats import poisson
 import simpy
-from SimComponents import PacketGenerator, PacketSink, SwitchPort
+from SimComponents import PacketGenerator, PacketSink, SwitchPort, link
 
 MU = 0.6 # Arrival distributed parameter mu for poisson inter arival time
-BANDWIDTH = 1000000 # 1mb/s
-QSIZE = 500000 # in bytes
-LAMBDA = 0.3 # e^(-0.3)x
+LINK_BANDWIDTH = 100
+SWITCH_BANDWIDTH = 100
+SWITCH_QSIZE = 50 # in bytes
+LAMBDA = .02 # e^(-0.3)x
 
 def constArrival():  # Constant arrival distribution for generator 1
     return int(poisson.rvs(MU)) # using Poisson interarrival time to model bursty traffic
@@ -14,14 +15,42 @@ def constArrival():  # Constant arrival distribution for generator 1
 def distSize():
     return expovariate(LAMBDA) # packet size distribution
 
+def delay(src, dst):
+    # TODO:
+    return #time delay between router i and router j
+
+def sigma(i, n):
+    return # agrigated service time of packet n at router i
+
+def M():
+    return # Matrix object
+
+def Mprime():
+    return # Matrix object
+
+def make_A(vn, n):
+    """ vn is the window size experienced by packet n
+        n is the packet number
+    """
+    return # A_vn 
+
 env = simpy.Environment()  
 # Create the SimPy environment
 # Create the packet generators and sink
-ps = PacketSink(env, debug=True)  # debugging enable for simple output
 pg = PacketGenerator(env, "Generator", constArrival, distSize)
-s1 = SwitchPort(env, rate=BANDWIDTH, qlimit=QSIZE)
-s2 = SwitchPort(env, rate=BANDWIDTH, qlimit=QSIZE)
+ps = PacketSink(env, debug=True)  # debugging enable for simple output
+l1 = link(env, LINK_BANDWIDTH)
+l2 = link(env, LINK_BANDWIDTH)
+l3 = link(env, LINK_BANDWIDTH)
+s1 = SwitchPort(env, rate=SWITCH_BANDWIDTH, qlimit=SWITCH_QSIZE)
+s2 = SwitchPort(env, rate=SWITCH_BANDWIDTH, qlimit=SWITCH_QSIZE)
 # Wire packet generators and sink together
+# pg.out = l1
+# l1.out = s1
+# s1.out = l2
+# l2.out = s2
+# s2.out = l3
+# l3.out = ps 
 pg.out = s1
 s1.out = s2
 s2.out = ps
