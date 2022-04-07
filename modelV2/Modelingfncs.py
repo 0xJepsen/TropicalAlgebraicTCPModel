17c1880559a3d09b79b446578a017e4bd723441d
Waylon Jepsen <waylonjepsen1@gmail.com>
2022-04-07 09:30:54 -0600

    modeling function for all Y complete and correct

--- modelV2/Modelingfncs.py
+++ modelV2/Modelingfncs.py
@@ -13,32 +13,48 @@ def initialize(number_of_packers, number_of_routers):
     return
 
 
-def Y_i(init, n, k):
-    # if i-1 == -1:
-    for j in range(0, n + 1):
-        # print("j: ", j)
-        for i in range(0, k + 1):
+def Y_i(init, number_of_packets, number_of_routers, max_window):
+    sent = 0
+    running_window = 1
+    for j in range(0, number_of_packets + 1):
+        print("Top j: ", j)
+        for i in range(0, number_of_routers + 1):
+            print("Top i: ", i)
             if i == 0:
                 if j == 0:
                     init[j] = [0]
+
                 else:
-                    init[j] = [init[j - 1][k] + delay(k, 0)]
+                    print("running Window: ", running_window)
+                    print("j: ", j)
+                    print("init[j-running window][number_of_routers]: ", init[j - running_window][number_of_routers])
+                    init[j] = [init[j - running_window][number_of_routers] + delay(number_of_routers, 0)]
+                    print("Sent: ", sent)
+                    sent +=1
+                    print("Sent after increment: ", sent)
             else:
-                # print("i: ", i)
-                # # print(init[i])
-                # print("init[n][i-1]: ", init[j][i-1])
-                init[j].append(init[j][i - 1] + delay(i - 1, i) + sigma())
+                # if init[j][0] == init[j-1][0]:
+                #     init[j].append(init[j][i - 1] + delay(i - 1, i) + sigma())
+                print("i: ", i)
+                print("j: ", j)
+                print("Time left last router: ", init[j][i - 1])
+                print("term one: ", init[j][i - 1] + delay(i - 1, i))
+                if j-1 == -1:
+                    the_max = max(init[j][i - 1] + delay(i - 1, i), 0)
+                else: the_max = max(init[j][i - 1] + delay(i - 1, i), init[j-1][i])
+                init[j].append(the_max + sigma())
+            print("Sent outside: ", sent)
+            if running_window == max_window:
+                running_window = 0
+            if sent == running_window +1:
+                running_window +=1
+                sent = 0
     return init
-    # print("n: ", n)
-    # if n == -1:
-    #     return 0
-    # print(y_i_minus_1 + 1)
-    # return max((y_i_minus_1 + 1), Y_i(y_i_minus_1, n - 1)) + 1
 
 
 vector = {}
 
 test_2_1 = [[0, 2, 4, 6], [9, 11, 13, 15], [9, 11, 13, 15]]
 
-result = Y_i(vector, 7, 3)
+result = Y_i(vector, 7, 3, 4)
 pprint(result)
