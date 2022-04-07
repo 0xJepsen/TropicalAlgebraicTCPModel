fcbd9b5ab8635b924e54ee49488d15ac3bbff5c7
Waylon Jepsen <waylonjepsen1@gmail.com>
2022-04-07 08:40:52 -0600

    Modeling Vn with constant window size = 1

--- modelV2/Modelingfncs.py
+++ modelV2/Modelingfncs.py
@@ -1,5 +1,44 @@
-# from matrixmath import Matrix
+from pprint import pprint
 
 
 def delay(src, dst):
     return abs(dst - src)  # for link rate of 1
+
+
+def sigma():
+    return 1
+
+
+def initialize(number_of_packers, number_of_routers):
+    return
+
+
+def Y_i(init, n, k):
+    # if i-1 == -1:
+    for j in range(0, n + 1):
+        # print("j: ", j)
+        for i in range(0, k + 1):
+            if i == 0:
+                if j == 0:
+                    init[j] = [0]
+                else:
+                    init[j] = [init[j - 1][k] + delay(k, 0)]
+            else:
+                # print("i: ", i)
+                # # print(init[i])
+                # print("init[n][i-1]: ", init[j][i-1])
+                init[j].append(init[j][i - 1] + delay(i - 1, i) + sigma())
+    return init
+    # print("n: ", n)
+    # if n == -1:
+    #     return 0
+    # print(y_i_minus_1 + 1)
+    # return max((y_i_minus_1 + 1), Y_i(y_i_minus_1, n - 1)) + 1
+
+
+vector = {}
+
+test_2_1 = [[0, 2, 4, 6], [9, 11, 13, 15], [9, 11, 13, 15]]
+
+result = Y_i(vector, 7, 3)
+pprint(result)
--- modelV2/main.py
+++ modelV2/main.py
@@ -1,7 +1,9 @@
 from random import expovariate
 import simpy
 from SimComponents import PacketGenerator, PacketSink, SwitchPort
+# from Modelingfncs import Y_i, delay
 import pandas as pd
+from pprint import pprint
 
 
 def constArrival():  # Constant arrival distribution for generator 1
@@ -14,39 +16,72 @@ def constArrival2():
 
 # def distSize():
 #     return expovariate(0.01)
-distSize =10
-SWITCH_BANDWIDTH =10
-LINK_BANDWIDTH =10
-SWITCH_QSIZE =50
+distSize = 10
+SWITCH_BANDWIDTH = 10
+LINK_BANDWIDTH = 10
+SWITCH_QSIZE = 50
 SIM_TIME = 75
 
 V_n = {1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 1, 1, 2, 2, 2, 3, 3, 3, 3}
 
-
 env = simpy.Environment()  # Create the SimPy environment
+
+
 # Create the packet generators and sink
-pg = PacketGenerator(
-    env, "Generator", constArrival, distSize, LINK_BANDWIDTH
-)
-s1 = SwitchPort(1, env, rate=SWITCH_BANDWIDTH, qlimit=SWITCH_QSIZE)
-s2 = SwitchPort(2, env, rate=SWITCH_BANDWIDTH, qlimit=SWITCH_QSIZE)
-ps = PacketSink(3, env, rate=SWITCH_BANDWIDTH, qlimit=SWITCH_QSIZE)
-
-# Wire packet generators and sink together
-pg.out = s1
-s1.out = s2
-s2.out = ps
-ps.out = pg
-env.run(until=SIM_TIME)
-
-# pprint(ps.data)
-df = pd.DataFrame.from_dict(ps.data)
-df = df.transpose()
-print(df)
-print(
-    "recieved: {}, s1 dropped {}, s2 dropped {}, sent {}".format(
-        ps.packets_rec, s1.packets_drop, s2.packets_drop, pg.packets_sent
+def main():
+    pg = PacketGenerator(
+        env, "Generator", constArrival, distSize, LINK_BANDWIDTH
     )
-)
-# print(df['departures'][0])
-y_k_0  = df['departures'][0]
+    s1 = SwitchPort(1, env, rate=SWITCH_BANDWIDTH, qlimit=SWITCH_QSIZE)
+    s2 = SwitchPort(2, env, rate=SWITCH_BANDWIDTH, qlimit=SWITCH_QSIZE)
+    ps = PacketSink(3, env, rate=SWITCH_BANDWIDTH, qlimit=SWITCH_QSIZE)
+
+    # Wire packet generators and sink together
+    pg.out = s1
+    s1.out = s2
+    s2.out = ps
+    ps.out = pg
+    env.run(until=SIM_TIME)
+
+    # pprint(ps.data)
+    df = pd.DataFrame.from_dict(ps.data)
+    df = df.transpose()
+    print(df)
+    print(
+        "recieved: {}, s1 dropped {}, s2 dropped {}, sent {}".format(
+            ps.packets_rec, s1.packets_drop, s2.packets_drop, pg.packets_sent
+        )
+    )
+    print(df['departures'][0][0])
+
+    # departure = [[df['departures'][0][0]]]
+    # print(departure[0][0])
+    # for i in range(0, 3):
+    #     print("i: ", i)
+    #     departure[0].append(Y_i(departure[0][i], 0))
+    #
+    # pprint(departure)
+    #
+    # packet_1 = [departure[0][3] + 3]
+    # for i in range(0,3):
+    #     packet_1.append(Y_i(packet_1[i], 1))
+    # pprint(packet_1)
+
+    # for j in range(0, ps.packets_rec -2):
+    #     for i in range(0, 3):
+    #         print("i: ", i)
+    #         departure[j].append(Y_i(departure[j][i], j))
+    #         if i == 2:
+    #             # print(departure[0][i+1])
+    #             departure.append([departure[0][i+1] + 3])
+    # print(departure)
+    # print(y_1_0)
+    # y_2_0 = Y_i(y_1_0, 0)
+    # print(y_2_0)
+    # y_3_0 = Y_i(y_2_0, 0)
+    # print(y_3_0)
+
+    # y_0_1 =
+
+if __name__ == '__main__':
+    main()
