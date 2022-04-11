96bb5aee24d5d7cb1cd56ed4fb1994db45a326b4
Waylon Jepsen <waylonjepsen1@gmail.com>
2022-04-11 09:57:59 -0600

    Fixed bug in modelingfnc
    Window size was not being adjusted accordingly, in the generated sequences

--- modelV2/Modelingfncs.py
+++ modelV2/Modelingfncs.py
@@ -32,12 +32,13 @@ def Make_Y(init, number_of_packets, number_of_routers, max_window):
         for i in range(0, number_of_routers + 1):
             if i == 0:
                 if j == 0:
-                    init[j] = {'departures': {0: 0}}
+                    init[j] = {'departures': {0: 0}, 'V_n': 1}
                     """first packet depart time"""
                 else:
                     """y_o(n) = Y_K(n - v_n-1) + d_(K,0)"""
                     init[j] = {'departures': {0: init[j - running_window]['departures'][number_of_routers] + delay(number_of_routers, 0)}}
                     sent += 1
+                    init[j]['V_n'] = running_window
             else:
                 if j - 1 == -1:
                     the_max = max(init[j]['departures'][i - 1] + delay(i - 1, i), 0)
@@ -46,20 +47,36 @@ def Make_Y(init, number_of_packets, number_of_routers, max_window):
                     the_max = max(init[j]['departures'][i - 1] + delay(i - 1, i), init[j - 1]['departures'][i])
                     """y_i(n) = [max(y_i-1(n) + d_(i-1,i), y_i(n-1)]"""
                 init[j]['departures'][i] = the_max + sigma()
-
-            if running_window == max_window:
-                running_window = 1
-            if sent == running_window + 1:
-                running_window += 1
-                sent = 0
+        if running_window == max_window:
+            running_window = 1
+            sent = 0
+        if sent == running_window + 1:
+            running_window += 1
+            sent = 0
     return init
 
+# def Make_Y(init, number_of_packets, number_of_routers, max_window):
+#     """ This generates the vectors Y(n) for each packet n consisting of the departure times
+#         of packet n from router i for 0<=i<=k where k is the number of routers.
+#         This implements equations
+#         y_o(n) = Y_K(n - v_n-1) + d_(K,0)
+#         y_i(n) = [max(y_i-1(n) + d_(i-1,i), y_i(n-1)] + sigma_i(n)
+#         Parameters
+#         ----------
+#         init : float
+#             the time the packet arrives at the output queue.
+#         number_of_packets : float
+#             the size of the packet in bytes
+#         number_of_routers : int
+#             an identifier for the packet
+#         max_window: int
+#             identifiers for source and destination
 
 vector = {}
 #
 # test_2_1 = [[0, 2, 4, 6], [9, 11, 13, 15], [9, 11, 13, 15]]
 #
-result = Make_Y(vector, 10, 3, 4)
+result = Make_Y(vector, 14, 3, 4)
 
 # for i in range (0,13):
 #     for j in range(0, 13):
--- modelV2/main.py
+++ modelV2/main.py
@@ -21,7 +21,7 @@ distSize = 10
 SWITCH_BANDWIDTH = 10
 LINK_BANDWIDTH = 10
 SWITCH_QSIZE = 50
-SIM_TIME = 40
+SIM_TIME = 200
 
 # V_n = {1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 1, 1, 2, 2, 2, 3, 3, 3, 3}
 
@@ -62,18 +62,18 @@ def main():
     )
     df = pd.DataFrame.from_dict(ps.data)
     df_simulated = df.transpose()
-    pprint(df_simulated.head())
+    pprint(df_simulated.iloc[0:12])
 
     """Logic for error extraction of Y_n"""
     df_simulated_departures = df_simulated.loc[:, ["departures"]]
     print("---------- Simulated Departure Data ----------")
-    pprint(df_simulated_departures.head())
+    # pprint(df_simulated_departures.head())
 
     init = {}
     generated_departures = Make_Y(init, ps.packets_rec - 1, 3, 4)
     df_generated = pd.DataFrame.from_dict(generated_departures, orient='index')
     print("---------- Generated Departure Data ----------")
-    pprint(df_generated.head())
+    pprint(df_generated.iloc[0:12])
 
 
     errors = {}
@@ -87,7 +87,7 @@ def main():
     df_errors["sum"] = df_errors.sum(axis=1)
 
     print("---------- Error In Departure Times ----------")
-    pprint(df_errors.head())
+    pprint(df_errors.iloc[8:13])
     ax = df_errors.plot()
     ax.set_ylabel('Quantity of Error')
     ax.set_xlabel('Packet Number')
