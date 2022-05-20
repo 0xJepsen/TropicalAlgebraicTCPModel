60d20c45187ed076280c4049ecd23ee0487fbcd8
Waylon Jepsen <waylonjepsen1@gmail.com>
2022-05-19 21:21:20 -0600

    stop light

---
+++ model/stoplightExample.py
@@ -0,0 +1,38 @@
+# Python 3 code to demonstrate basics of SimPy package
+# Simulation of a Traffic Light
+
+# import the SimPy package
+import simpy
+
+
+# Generator function that defines the working of the traffic light
+# "timeout()" function makes next yield statement wait for a
+# given time passed as the argument
+def Traffic_Light(env):
+    while True:
+        print("Light turns GRN at " + str(env.now))
+
+        # Light is green for 25 seconds
+        yield env.timeout(25)
+
+        print("Light turns YEL at " + str(env.now))
+
+        # Light is yellow for 5 seconds
+        yield env.timeout(5)
+
+        print("Light turns RED at " + str(env.now))
+
+        # Light is red for 60 seconds
+        yield env.timeout(60)
+
+    # env is the environment variable
+
+
+env = simpy.Environment()
+
+# The process defined by the function Traffic_Light(env)
+# is added to the environment
+env.process(Traffic_Light(env))
+
+# The process is run for the first 180 seconds (180 is not included)
+env.run(until=180)
